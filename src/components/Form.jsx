1d807c34462b62358e5f6c45182e780dacf2c66b
Natália Trindade <natalia.santos@germinare.org.br>
2025-10-18 19:07:27 -0300

    adicionar notificaçao de sucesso de cadastro

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
+import { ToastContainer, toast } from 'react-toastify'
+import "react-toastify/dist/ReactToastify.css";
 
 const Form = () => {
+    const notificacaoSucesso = () => toast.success("Pokémon cadastrado!");
+
     const [nome, setNome] = useState("");
     const [tipo, setTipo] = useState("");
     const [descricao, setDescricao] = useState("");
@@ -15,72 +19,77 @@ const Form = () => {
         setTipo("");
         setDescricao("");
         setPoder("");
+        notificacaoSucesso();
       };
 
-    return (
+      return (
         <div>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="nome">Nome do Pokémon </label>
-                    <input
-                        type="text"
-                        name="nome"
-                        placeholder="Pokémon"
-                        onChange={(e) => { setNome(e.target.value) }
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="nome">Nome do Pokémon </label>
+              <input
+                type="text"
+                name="nome"
+                placeholder="Pokémon"
+                onChange={(e) => { setNome(e.target.value) }
                     }
-                        value={nome}
-                    />
-                </div>
-
-                <label>
-                    Tipo
-                    <select name="role" onChange={(e) => setRole(e.target.value)}>
-                    <option value="normal">Normal</option>
-                    <option value="fogo">Fogo</option>
-                    <option value="agua">Água</option>
-                    <option value="grama">Grama</option>
-                    <option value="elétrico">Elétrico</option>
-                    <option value="gelo">Gelo</option>
-                    <option value="lutador">Lutador</option>
-                    <option value="veneno">Veneno</option>
-                    <option value="terra">Terra</option>
-                    <option value="voador">Voador</option>
-                    <option value="psíquico">Psíquico</option>
-                    <option value="inseto">Inseto</option>
-                    <option value="pedra">Pedra</option>
-                    <option value="fantasma">Fantasma</option>
-                    <option value="dragão">Dragão</option>
-                    <option value="sombrio">Sombrio</option>
-                    <option value="aço">Aço</option>
-                    <option value="fada">Fada</option>
-                    </select>
-                </label>
-
-                <div>
-                    <label htmlFor="descricao">Descrição </label>
-                    <input
-                        type="text"
-                        name="descricao"
-                        placeholder="Descrição"
-                        onChange={(e) => { setDescricao(e.target.value) }
+                value={nome}
+              />
+            </div>
+    
+            <label>
+              Tipo
+              <select name="role" onChange={(e) => setRole(e.target.value)}>
+                <option value="normal">Normal</option>
+                <option value="fogo">Fogo</option>
+                <option value="agua">Água</option>
+                <option value="grama">Grama</option>
+                <option value="elétrico">Elétrico</option>
+                <option value="gelo">Gelo</option>
+                <option value="lutador">Lutador</option>
+                <option value="veneno">Veneno</option>
+                <option value="terra">Terra</option>
+                <option value="voador">Voador</option>
+                <option value="psíquico">Psíquico</option>
+                <option value="inseto">Inseto</option>
+                <option value="pedra">Pedra</option>
+                <option value="fantasma">Fantasma</option>
+                <option value="dragão">Dragão</option>
+                <option value="sombrio">Sombrio</option>
+                <option value="aço">Aço</option>
+                <option value="fada">Fada</option>
+              </select>
+            </label>
+    
+            <div>
+              <label htmlFor="descricao">Descrição </label>
+              <input
+                type="text"
+                name="descricao"
+                placeholder="Descrição"
+                onChange={(e) => { setDescricao(e.target.value) }
                     }
-                        value={descricao}
-                    />
-                </div>
-
-                <div>
-                    <label htmlFor="poder">Poder </label>
-                    <input
-                        type="number"
-                        name="poder"
-                        placeholder="0-100"
-                        max="100" min="0"
-                        onChange={(e) => { setPoder(e.target.value) }
+                value={descricao}
+              />
+            </div>
+    
+            <div>
+              <label htmlFor="poder">Poder </label>
+              <input
+                type="number"
+                name="poder"
+                placeholder="0-100"
+                max="100" min="0"
+                onChange={(e) => { setPoder(e.target.value) }
                     }
-                        value={poder}
-                    />
-                </div>
-            </form>
+                value={poder}
+              />
+            </div>
+    
+            <button type="submit">Cadastrar Pokémon</button>
+          </form>
+    
+          <ToastContainer />
         </div>
     )
 }
